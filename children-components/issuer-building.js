41163a8935602098bce0cdb7e7ad5cc4b12f14fb
llopezampiez7 <llopezampiez7@gmail.com>
2025-10-29 17:23:12 -0400

    Add IssuerBuilding and IssuerInformation components with styled rendering

---
+++ children-components/issuer-building.js
@@ -0,0 +1,30 @@
+import { StyledComponent } from '../base-components/styled-component.js';
+
+export class IssuerBuilding extends StyledComponent {
+  static getCustomAttributes() {
+    return ['name'];
+  }
+
+  render() {
+    const name = this.getAttribute('name') || '';
+
+    this.shadowRoot.innerHTML = `
+      <style>
+        span {
+          font-size: ${this.fontSize};
+          font-weight: ${this.fontWeight};
+          font-family: ${this.fontFamily};
+          padding: ${this.padding};
+          margin: ${this.margin};
+          background: ${this.background};
+          border: ${this.border};
+          border-radius: ${this.borderRadius};
+          color: ${this.color};
+        }
+      </style>
+      <span>${name}</span>
+    `;
+  }
+}
+
+customElements.define('issuer-building', IssuerBuilding);
